e48dc330d7cc1ecd23d22c628048a2632e743ccd
razparnafes <34274974+razparnafes@users.noreply.github.com>
2022-07-26 10:47:32 +0300

    Update normalizers.py
    updates to chiefs code

--- normalizers.py
+++ normalizers.py
@@ -1,5 +1,6 @@
 import torch
 
+
 class RadiationNormalizer(object):
     def __init__(self, radiation_data_loader, start_time, end_time):
         self._radiation_data_loader = radiation_data_loader
